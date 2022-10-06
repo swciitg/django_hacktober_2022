4c9528f5345627b28a726935f1e44601df04b4fc
Arpit Sureka <surekaarpit@gmail.com>
2022-10-07 01:18:18 +0530

    changes

--- sac_management_system/sac_management_system/urls.py
+++ sac_management_system/sac_management_system/urls.py
@@ -1,4 +1,4 @@
-"""sac_management_sys URL Configuration
+"""sac_management_system URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/

95bb7dde63563c6b6966bff5a5b13e0d8bce26ea
Arpit Sureka <surekaarpit@gmail.com>
2022-10-07 16:23:20 +0530

    small changes

--- sac_management_system/student/models.py
+++ sac_management_system/student/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 
-# Create your models here.
-
-
-
-# Create your models here.
 class Student(models.Model):
+
     Full_Name = models.TextField()
     Email = models.TextField()
     Roll_Number = models.IntegerField()
     Student_No = models.IntegerField()
     Temp_Permissions = models.IntegerField()
     Perm_Permissions = models.IntegerField()
+    
     def _str_(self):
         return self.Student
+
 class FutureBookings(models.Model):
+
     RoomNo = models.IntegerField()
     Student = models.IntegerField()
     Use = models.TextField()
     From = models.DateTimeField()
     Till = models.DateTimeField()
+    
     def _str_(self):
         return self.FutureBookings

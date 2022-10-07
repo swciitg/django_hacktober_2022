6d286f5dd4a2894f30e2e62530b068534aa448c2
Manideep <sankooru@iitg.ac.in>
2022-10-07 16:04:23 +0530

    added_models

--- sac_management_system/rooms/models.py
+++ sac_management_system/rooms/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Rooms(models.Model):
+    class Type(models.TextChoices):
+        PUBLIC = 'Public',
+        CLUB = 'Club',
+
+    Room_Name = models.TextField()
+    Room_Location = models.TextField()
+    Room_No = models.IntegerField()
+    Room_Details = models.TextField()
+    Who_Has_Key_Currently = models.IntegerField()
+    type = models.TextField(choices = Type.choices, default = Type.PUBLIC)
+    def _str_(self):
+        return self.Rooms
+class LogEntry(models.Model):
+    class Stage(models.TextChoices):
+        RETURNED = 'Returned',
+        TAKEN = 'Taken',
+        REQUESTED = 'Requested',
+    Room_No = models.IntegerField()
+    Key_Taken = models.DateTimeField()
+    Key_Given = models.DateTimeField()
+    Student_ID = models.TextField()
+    stage = models.TextField(choices = Stage.choices)
+    def _str_(self):
+        return self.LogEntry
--- sac_management_system/student/admin.py
+++ sac_management_system/student/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+# Register your models here.
+from . models import Student
+from . models import FutureBookings
+admin.site.register(Student)
+admin.site.register(FutureBookings)

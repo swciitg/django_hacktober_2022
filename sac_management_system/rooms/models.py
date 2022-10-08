f079a410cf0ff0898336ddf7acc254998a59b423
Manideep <sankooru@iitg.ac.in>
2022-10-08 22:33:21 +0530

    IMPROVED_MODELS

--- sac_management_system/rooms/models.py
+++ sac_management_system/rooms/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from student.models import Student
 
 class Type(models.TextChoices):
     PUBLIC = 'Public',
@@ -10,15 +11,13 @@ class Stage(models.TextChoices):
     RETURNED = 'Returned',
     TAKEN = 'Taken',
     REQUESTED = 'Requested',
-
-
 class Rooms(models.Model):
 
     Room_Name = models.TextField()
     Room_Location = models.TextField()
-    Room_No = models.IntegerField()
-    Room_Details = models.TextField()
-    Who_Has_Key_Currently = models.IntegerField()
+    Room_No = models.IntegerField(primary_key=True)
+    Room_Details = models.TextField(null=True)
+    Who_Has_Key_Currently = models.ForeignKey(Student, on_delete=models.CASCADE,null=True)
     type = models.TextField(choices=Type.choices, default=Type.PUBLIC)
 
     def _str_(self):
@@ -27,11 +26,14 @@ class Rooms(models.Model):
 
 class LogEntry(models.Model):
 
-    Room_No = models.IntegerField()
-    Key_Taken = models.DateTimeField()
+    Room_No = models.ForeignKey(Rooms, on_delete = models.CASCADE)
+    Key_Taken = models.DateTimeField(null=True)
     Key_Given = models.DateTimeField()
-    Student_ID = models.TextField()
+    Student_ID = models.IntegerField(Student, on_delete = models.CASCADE)
     stage = models.TextField(choices=Stage.choices)
 
     def _str_(self):
         return self.LogEntry
+
+
+
--- sac_management_system/student/models.py
+++ sac_management_system/student/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 
+from rooms.models import Rooms
 class Student(models.Model):
 
     Full_Name = models.TextField()
-    Email = models.TextField()
-    Roll_Number = models.IntegerField()
-    Student_No = models.IntegerField()
-    Temp_Permissions = models.IntegerField()
-    Perm_Permissions = models.IntegerField()
+    Email = models.TextField(unique=True)
+    Roll_Number = models.IntegerField(max_length=9)
+    Student_No = models.IntegerField(primary_key=True)
+    Temp_Permissions = models.ForeignKey(Rooms, on_delete = models.CASCADE)
+    Perm_Permissions = models.ForeignKey(Rooms, on_delete = models.CASCADE)
     
     def _str_(self):
         return self.Student
 
 class FutureBookings(models.Model):
 
-    RoomNo = models.IntegerField()
-    Student = models.IntegerField()
+    RoomNo = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    Student = models.TextField(Student, on_delete = models.CASCADE)
     Use = models.TextField()
     From = models.DateTimeField()
     Till = models.DateTimeField()
